923fb0616c31bee3929d8b0ab0e4b95da2362830
Jay Sharma <Mailtojaysharma@gmail.com>
2022-05-19 11:37:02 +0530

    Updated game logic

--- src/hook/useWordle.js
+++ src/hook/useWordle.js
@@ -1,64 +1,6 @@
-// import { useState } from 'react'
-
-// const useWordle = (solution) => {
-
-//  
-
-//     const [turn, setTurn] = useState(0) 
-
-
-
-//      const [currentGuess, setCurrentGuess] = useState('')
-//     // each guess is an array each guess stored here is a formatted guess 
-
-//     const [guesses, setGuesses] = useState([])
-
-
-
-//     const [history, setHistory] = useState(['hello', 'world']) // each guess is a string
-
-
-
-//     const [isCorrect, setIsCorrect] = useState(false)
-
-// //     // format a guessed word into a array of letter object
-// //     // [key: 'a', color: 'Green'}]
-
-
-//     const formatGuess = ()=>
-//     {
-//     console.log('Formatting The Guess -- ', currentGuess)
-//     }
-
-
-
-//     const addNewGuess= () => {
-
-//     }
-
-
-          
-// const handleKeyup = ({ key }) => 
-// {
-//     if(key === 'Enter')
-//     {
-//         // Only add guess if less than 5
-//      if(turn > 5)
-//     {
-//       console.log("You Used All Your Chances to Guess")
-//       return 
-//     }
-//     if(history.includes(currentGuess))
-//     {
-//      console.log("You Alreadt Tried this Word");
-//      return
-//     }     
-//     }
-
 import { useState } from 'react'
 
 const useWordle = (solution) => {
-
 
   // it starts with 0 and goes upto 6 guesses that the user makes
   const [turn, setTurn] = useState(0) 
@@ -66,12 +8,12 @@ const useWordle = (solution) => {
   // this is for tracking what the user is typing in the current guess and
   // it gets updated everytime the user hits a new key on the keyboard(this used the hadle key function)
   const [currentGuess, setCurrentGuess] = useState('')
-  const [guesses, setGuesses] = useState([]) // each guess is an array
+  // each guess is an array
+  const [guesses, setGuesses] = useState([]) 
 
-
-      // this is for storing the past guesses of the user
-    // this is used to check if user does not type the same guess word again
-  const [history, setHistory] = useState(['hello', 'ninja']) // each guess is a string
+// this is for storing the past guesses of the user
+// this is used to check if user does not type the same guess word again
+  const [history, setHistory] = useState(['hello', 'world']) // each guess is a string
  
   
   // show only when the user wins the game
@@ -81,11 +23,28 @@ const useWordle = (solution) => {
   // format a guess into an array of letter objects 
   // e.g. [{key: 'a', color: 'yellow'}]
 
-//     // this function will console only if the current guess is correct
+
+  // this function will console only if the current guess is correct and not been used previously
 
   const formatGuess = () => 
   {
     console.log('formatting the guess - ', currentGuess)
+
+    // converting the string into the array of  individual (spread operator)
+    let solutionArray = [...solution] 
+
+    //making each letter to be an object where it have a key property which will be the letter itself and a colour property
+    
+    // map will map this array into a new array
+
+    // map will fire a function for each letter in the array and we get access to that array of letter on each itteration
+
+    let formattedGuess = [...currentGuess].map((l) => {
+      return {key: l, color: 'grey'}
+    })
+
+
+
   }
 
 //     //add new guess to the guess state
